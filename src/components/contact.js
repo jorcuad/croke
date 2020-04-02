123cf493d31db24c82eaa7deede8e5b02018373f
Croke <jorgecuadradosaez@gmail.com>
2020-04-02 12:40:46 +0200

    form styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,7 +7,7 @@ const Wrapper = styled.div`
 `;
 
 const Contact = styled.form`
-  ${tw`flex w-full`}
+  ${tw`flex w-full bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4`}
 `;
 
 const Fields = styled.div`
@@ -15,23 +15,23 @@ const Fields = styled.div`
 `;
 
 const UpperFields = styled.div`
-  ${tw`flex flex-row justify-between w-full mb-4`}
+  ${tw`flex flex-row justify-between w-full mb-4 justify-between`}
 `;
 
 const Name = styled.input`
-  ${tw`flex bg-croke-green`}
+  ${tw`flex w-5/12 shadow appearance-none border border-croke-purple rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
 `;
 
 const Mail = styled.input`
-  ${tw`flex bg-croke-green`}
+  ${tw`flex w-6/12 shadow appearance-none border border-croke-purple rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
 `;
 
 const Message = styled.textarea`
-  ${tw`flex h-32 bg-croke-purple mb-4`}
+  ${tw`flex h-32 mb-4 shadow appearance-none border border-croke-purple rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
 `;
 
 const Send = styled.button`
-  ${tw`flex bg-croke-green justify-center w-1/5`}
+  ${tw`flex justify-center w-1/5 bg-croke-purple hover:bg-croke-green text-croke-white hover:text-croke-purple font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
 `;
 
 export default props => (
