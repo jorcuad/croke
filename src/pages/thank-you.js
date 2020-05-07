73c000adf75b66ab69eaf0e07b2676f411ac176d
Croke <jorgecuadradosaez@gmail.com>
2020-05-08 00:49:41 +0200

    Thanks page

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -1,11 +1,34 @@
-import React from "react"
+import React from 'react';
+import {
+  SectionTitle,
+  SectionWrapper,
+  SectionText,
+} from '../components/section';
 import Layout from "../components/layout/layout"
+import Popular from "../components/popular";
+import Areas from "../components/areas";
 
-const ThankYouPage = () => (
+
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+
+const thanks = "Thank you for you interest and support, this simple interactions encourage me to continue this and other projects. I will try to contact you back soon. Meanwhile, you can take a look in the rest of the webpage, I will try to continue upgrading and updating the content so stay tuned."
+
+const Wrapper = styled.div`
+  ${tw`flex flex-col relative w-screen items-center justify-center`}
+`;
+export default () => (
   <Layout>
-    <h1>Contact</h1>
-    <p>Thank you for your submission!</p>
+    <Wrapper>
+      <SectionWrapper>
+        <SectionTitle header="The email has been sended. Thanks for your support."/>
+        <SectionText content={thanks}/>
+        <Areas/>
+      </SectionWrapper>
+      <SectionWrapper>
+        <SectionTitle header="Most Popular Posts"/>
+        <Popular/>
+      </SectionWrapper>
+    </Wrapper>
   </Layout>
 )
-
-export default ThankYouPage
