56361c74b44e58d785ca5ca7a33fadee6ef383fa
Croke <jorgecuadradosaez@gmail.com>
2020-05-08 00:20:13 +0200

    chapta test

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,9 +2,38 @@ import React from "react";
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
 import { navigate } from 'gatsby-link'
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Wrapper = styled.div`
   ${tw`flex content-center w-full`}
+
+  .contact {
+    ${tw`flex w-full bg-white rounded px-8 pt-6 pb-8 mb-4`}
+  }
+
+  .fields {
+    ${tw`flex flex-col justify-between w-full`}
+  }
+
+  .upperField {
+    ${tw`flex flex-row justify-between w-full mb-4 justify-between`}
+  }
+
+  .name {
+    ${tw`flex w-5/12 shadow appearance-none border border-croke-purple rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+  }
+
+  .mail {
+    ${tw`flex w-6/12 shadow appearance-none border border-croke-purple rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+  }
+
+  .message {
+    ${tw`flex h-32 mb-4 shadow appearance-none border border-croke-purple rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+  }
+
+  .send {
+    ${tw`flex justify-center bg-croke-purple hover:bg-croke-green text-croke-white hover:text-croke-purple font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+  }
 `;
 
 const ContactForm = styled.form`
@@ -15,7 +44,7 @@ const Fields = styled.div`
   ${tw`flex flex-col justify-between w-full`}
 `;
 
-const UpperFields = styled.div`
+const LineFields = styled.div`
   ${tw`flex flex-row justify-between w-full mb-4 justify-between`}
 `;
 
@@ -55,16 +84,22 @@ const Send = styled.button`
 </Wrapper>*/}
 
 export default () => (
-    <form name="Contact Form" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="Contact Form"/>
-      <div>
-        <label>Your Email:</label>
-        <input type="email" name="email" />
+  <Wrapper>
+    <form className="contact" name="Contact Form" method="POST" data-netlify="true" action="/thank-you">
+      <div className="fields">
+        <input type="hidden" name="form-name" value="Contact Form"/>
+        <LineFields>
+          <input className="mail" type="email" name="email" placeholder="Your Email"/>
+          <input className="name" type="email" name="email" placeholder="Your Name"/>
+        </LineFields>
+        <textarea className="message" name="message" placeholder="Thanks for you interest."/>
+        <LineFields>
+          <ReCAPTCHA sitekey={process.env.GATSBY_RECAPTCHA_KEY} />
+          <div>
+            <button className="send" type="submit">Send</button>
+          </div>
+        </LineFields>
       </div>
-      <div>
-        <label>Message:</label>
-        <textarea name="message" />
-      </div>
-      <button type="submit">Send</button>
     </form>
+  </Wrapper>
 )
